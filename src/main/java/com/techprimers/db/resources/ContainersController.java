46866fe08476d89661cd1462f8553d7b6031b940
Tyler <malonet@g.cofc.edu>
2020-03-10 08:43:01 -0700

    fully working dynamic table... no refresh

--- src/main/java/com/techprimers/db/resources/ContainersController.java
+++ src/main/java/com/techprimers/db/resources/ContainersController.java
@@ -56,16 +56,37 @@ public class ContainersController {
 
     @PostMapping("/edit")
     public ModelAndView edit(HttpServletRequest req) throws ParseException {
+        /*
+        String begin_date = req.getParameter("dateBeginning");
+        Date from_date = new SimpleDateFormat("yyyy/MM/dd").parse(begin_date);
+        String end_date = req.getParameter("dateEnd");
+        Date to_date = new SimpleDateFormat("yyyy/MM/dd").parse(end_date);
+        Integer crane_number = Integer.parseInt(req.getParameter("craneNumber"));
+        Integer old_ship_number = Integer.parseInt(req.getParameter("cVesselNumber"));
+        Integer new_ship_number = Integer.parseInt(req.getParameter("nVesselNumber"));
+        Integer old_badge_number = Integer.parseInt(req.getParameter("cBadgeNumber"));
+        Integer new_badge_number = Integer.parseInt(req.getParameter("nBadgeNumber"));
+        List<Containers> containersList = containersRepository.findByTransmittedDatetimeBetween(from_date, to_date, old_badge_number, old_ship_number, crane_number);
+        */
+        System.out.println("**************************");
         String begin_date = req.getParameter("begin_date");
         Date from_date = new SimpleDateFormat("yyyy/MM/dd").parse(begin_date);
         String end_date = req.getParameter("end_date");
         Date to_date = new SimpleDateFormat("yyyy/MM/dd").parse(end_date);
+        System.out.println("from_date: " + from_date);
+        System.out.println("to_date: " + to_date);
         Integer crane_number = Integer.parseInt(req.getParameter("crane_number"));
-        Integer old_ship_number = Integer.parseInt(req.getParameter("old_ship_number"));
+        System.out.println("crane_number: " + crane_number);
+        Integer old_ship_number = Integer.parseInt(req.getParameter("current_ship_number"));
+        System.out.println("old_ship_number: " + old_ship_number);
         Integer new_ship_number = Integer.parseInt(req.getParameter("new_ship_number"));
-        Integer old_badge_number = Integer.parseInt(req.getParameter("old_badge_number"));
+        System.out.println("new_ship_number: " + new_ship_number);
+        Integer old_badge_number = Integer.parseInt(req.getParameter("current_badge_number"));
+        System.out.println("current_badge_number: " + old_badge_number);
         Integer new_badge_number = Integer.parseInt(req.getParameter("new_badge_number"));
+        System.out.println("new_badge_number: " + new_badge_number);
         List<Containers> containersList = containersRepository.findByTransmittedDatetimeBetween(from_date, to_date, old_badge_number, old_ship_number, crane_number);
+        
         Integer number_of_updated_entries = 0;
         for (Containers container : containersList){
             container.setShipNumber(new_ship_number);
