92301ec277fb6ecb4df85ef8281d7ee67128080a
Tyler <malonet@g.cofc.edu>
2020-03-14 14:33:20 -0700

    2 forms, working together (was a nightmare)

--- src/main/java/com/techprimers/db/resources/ContainersController.java
+++ src/main/java/com/techprimers/db/resources/ContainersController.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+
 //import sun.util.calendar.BaseCalendar.Date;
 
 import java.util.Date;
@@ -16,6 +18,8 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.ui.Model;
+
+
 import java.util.List;
 
 @Controller
@@ -38,11 +42,14 @@ public class ContainersController {
     }
 
     @GetMapping("/show")
-    public ModelAndView show(Model model) {
-        List<Containers> containers = containersRepository.findAll();
-        // ModelAndView mav = new ModelAndView();
-        model.addAttribute("containers", containers);
-        return new ModelAndView("show", "users", model);
+    public ModelAndView show(Model model) throws ParseException {
+       
+        //model.addAttribute("containers", containers);
+        ModelAndView mav = new ModelAndView();
+        //mav.addObject("containers", containers);
+        mav.setViewName("show");
+        return mav;
+       // return new ModelAndView("show", "users", model);
     }
 
     @GetMapping("/productivity")
@@ -53,9 +60,46 @@ public class ContainersController {
         mav.setViewName("productivity");
         return mav;
     }
+    
+    @PostMapping("/retrieve")
+    @ResponseBody
+    public String retrieve(HttpServletRequest req) throws ParseException {
+        System.out.println("---------- IN RETRIEVE ----------");
+        String begin_date = req.getParameter("begin_date");
+        Date from_date = new SimpleDateFormat("yyyy/MM/dd").parse(begin_date);
+        String end_date = req.getParameter("end_date");
+        Date to_date = new SimpleDateFormat("yyyy/MM/dd").parse(end_date);
+        System.out.println("from_date: " + from_date);
+        System.out.println("to_date: " + to_date);
+        Integer crane_number = Integer.parseInt(req.getParameter("crane_number"));
+        System.out.println("crane_number: " + crane_number);
+        Integer old_ship_number = Integer.parseInt(req.getParameter("current_ship_number"));
+        System.out.println("old_ship_number: " + old_ship_number);
+        Integer old_badge_number = Integer.parseInt(req.getParameter("current_badge_number"));
+        System.out.println("current_badge_number: " + old_badge_number);
+        List<Containers> containersList = containersRepository.findByTransmittedDatetimeBetween(from_date, to_date, old_badge_number, old_ship_number, crane_number);
+        if (containersList.isEmpty())
+            System.out.println("************************list is empty!");
+        else 
+            System.out.println("NOT EMPTY!");
+        String return_string = "<thead><tr><th>Crane Number</th><th>Ship Number</th><th>Badge Number</th><th>Transmitted Datetime</th><th>Driver Shift Number</th></tr></thead><tbody><div id='retrieved'>";
+        for (Containers container : containersList) {
+            return_string = return_string + "<tr><td>" + container.getCraneNumber() + "</td>";
+            return_string = return_string + "<td>" + container.getShipNumber() + "</td>";
+            return_string = return_string + "<td>" + container.getBadgeNumber() + "</td>";
+            return_string = return_string + "<td>" + container.getTransmittedDatetime() + "</td>";
+            return_string = return_string + "<td>" + container.getDriverShiftNumber() + "</td></tr>";
+        }
+        return_string = return_string + "</div></tbody>";
+        System.out.println("return_string: " + return_string);
+        return return_string;
+    }
+
+
 
     @PostMapping("/edit")
-    public ModelAndView edit(HttpServletRequest req) throws ParseException {
+    @ResponseBody
+    public List<Object> edit(HttpServletRequest req) throws ParseException {
         /*
         String begin_date = req.getParameter("dateBeginning");
         Date from_date = new SimpleDateFormat("yyyy/MM/dd").parse(begin_date);
@@ -92,12 +136,21 @@ public class ContainersController {
             containersRepository.save(container);
             number_of_updated_entries = number_of_updated_entries + 1;
         }
-        List<Containers> containers = containersRepository.findAll();
-        ModelAndView mav = new ModelAndView();
-        mav.addObject("containers", containers);
-        mav.addObject("updated_entries", number_of_updated_entries);
-        mav.setViewName("show");
-        return mav;
+        String return_string = "<thead><tr><th>Crane Number</th><th>Ship Number</th><th>Badge Number</th><th>Transmitted Datetime</th><th>Driver Shift Number</th></tr></thead><tbody><div id='retrieved'>";
+        for (Containers container : containersList) {
+            return_string = return_string + "<tr><td>" + container.getCraneNumber() + "</td>";
+            return_string = return_string + "<td>" + container.getShipNumber() + "</td>";
+            return_string = return_string + "<td>" + container.getBadgeNumber() + "</td>";
+            return_string = return_string + "<td>" + container.getTransmittedDatetime() + "</td>";
+            return_string = return_string + "<td>" + container.getDriverShiftNumber() + "</td></tr>";
+        }
+        return_string = return_string + "</div></tbody>";
+        System.out.println("return_string: " + return_string);
+        List<Object> return_list = new ArrayList<Object>();
+        return_list.add(return_string);
+        return_list.add(number_of_updated_entries);
+        System.out.println("---------- RETURNING ----------");
+        return return_list;
     }
 
     @PostMapping("/create")
