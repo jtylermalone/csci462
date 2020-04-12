afec7ea9b0a7ae8abfa3ae2c7b7ab79a9e7066f6
Tyler <malonet@g.cofc.edu>
2020-04-12 07:24:17 -0700

    productivity javascript mostly working

--- src/main/java/com/scpa/db/repository/EmployeesRepository.java
+++ src/main/java/com/scpa/db/repository/EmployeesRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface EmployeesRepository extends JpaRepository<Employees, Integer> {
     List<Employees> findByEmpnum(String empnum);
 
-    @Query(value = "SELECT * FROM employees e WHERE e.DEPT=12;", nativeQuery = true)
+    @Query(value = "SELECT * FROM employees e WHERE e.DEPT=12 ORDER BY e.NAME", nativeQuery = true)
     List<Employees> findEmployees();
 
     @Query(value = "SELECT e.empnum FROM employees e WHERE e.NAME = :full_name", nativeQuery = true)
